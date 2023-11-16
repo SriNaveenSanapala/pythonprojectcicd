c2ffb3d737f5c45dcb586f9a82079b589c975374
SriNaveenSanapala <150837674+SriNaveenSanapala@users.noreply.github.com>
2023-11-16 23:48:47 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
+# Get the IDs of running containers publishing port 8000
+#running_containers=$(sudo docker ps -q --filter "publish=8000/tcp")
 
-# Stop the running container (if any)
-echo "Hi"
+# Output the IDs
+#echo "Containers to stop:"
+#echo "$running_containers"
+
+# Stop each container
+#while IFS= read -r container_id; do
+    echo "Stopping container $container_id..."
+    sudo docker stop "$container_id"
+#done <<< "$running_containers"

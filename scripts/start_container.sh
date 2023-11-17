90f346224c0f66985d6f36c0f3ae8c8f74ae15d8
SriNaveenSanapala <150837674+SriNaveenSanapala@users.noreply.github.com>
2023-11-17 06:30:25 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,10 +2,11 @@
 set -e
 
 # Authenticate Docker with ECR
-/usr/bin/aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c3x0j4u6
+/full/path/to/aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/c3x0j4u6
 
 # Pull the Docker image from ECR
-/usr/bin/docker pull public.ecr.aws/c3x0j4u6/naveen_ers:latest
+docker pull public.ecr.aws/c3x0j4u6/naveen_ers:latest
 
-# Run the Docker image as a containers
-/usr/bin/docker run -d -p 5000:5000  public.ecr.aws/c3x0j4u6/naveen_ers:latest
+# Run the Docker image as a container
+docker run -d -p 5000:5000 public.ecr.aws/c3x0j4u6/naveen_ers:latest
+

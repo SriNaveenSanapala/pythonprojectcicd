82bcf7c830cb29e1a01a54d3f60416c413627600
SriNaveenSanapala <150837674+SriNaveenSanapala@users.noreply.github.com>
2023-11-17 10:43:59 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop and remove the running container
+docker stop $(docker ps -q --filter ancestor=public.ecr.aws/c3x0j4u6/naveen_ers:latest)
+docker rm $(docker ps -a -q --filter ancestor=public.ecr.aws/c3x0j4u6/naveen_ers:latest)

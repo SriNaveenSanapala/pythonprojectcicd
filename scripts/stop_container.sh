718b2ef38c4f1b03c046612a832618d560a35d40
SriNaveenSanapala <150837674+SriNaveenSanapala@users.noreply.github.com>
2023-11-17 17:16:17 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,15 +2,16 @@
 set -e
 
 # Find the ID of the running container by filtering it based on your application image
-CONTAINER_ID=$(docker ps -q --filter ancestor=public.ecr.aws/c3x0j4u6/naveen_ers:latest)
+CONTAINER_ID=$(sudo docker ps -q --filter ancestor=public.ecr.aws/c3x0j4u6/naveen_ers:latest)
+
+# Print the container ID for debugging
+echo "Container ID: $CONTAINER_ID"
 
 # Check if the container is running
 if [ -n "$CONTAINER_ID" ]; then
-    # Print the container ID for debugging
-    echo "Container ID: $CONTAINER_ID"
-    
     # Stop the running container
-    docker stop "$CONTAINER_ID"
+    echo "Stopping container..."
+    sudo docker stop "$CONTAINER_ID"
     
     # Additional debugging information (optional)
     echo "Stopped container with ID: $CONTAINER_ID"
